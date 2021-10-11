ad9d6081b05b8ec15b85da81f7670d042b96547b
TimKie <55922216+TimKie@users.noreply.github.com>
2021-10-11 11:00:41 +0200

    Initial Commit

---
+++ BSP5/BSP5App.swift
@@ -0,0 +1,17 @@
+//
+//  BSP5App.swift
+//  BSP5
+//
+//  Created by Tim Kieffer on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BSP5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BSP5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BSP5
+//
+//  Created by Tim Kieffer on 11/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
